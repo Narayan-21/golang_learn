db240473f3f198c33ca27942feaacea7acca708b
Narayan-21 <naaidjan.19@gmail.com>
2024-08-17 09:57:02 +0530

    go test

--- hello_world.go
+++ hello_world.go
@@ -1,10 +1,16 @@
+// go run hello_world.go => Run the script
+// go build hello_world.go => build the binary of the script and then ./hello-world to execute that binary
+
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("hello world")
+func hello() string {
+	return "hello world"
 }
 
-// go run hello_world.go => Run the script
-// go build hello_world.go => build the binary of the script and then ./hello-world to execute that binary
+func main() {
+	fmt.Println(hello())
+}
+
+// separation of side effects from the domain code
