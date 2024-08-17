6dd23a0e38cf0e261612e31cf8d85258f588df43
Narayan-21 <naaidjan.19@gmail.com>
2024-08-17 09:43:01 +0530

    hello world

---
+++ hello_world.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// go run hello_world.go => Run the script
+// go build hello_world.go => build the binary of the script and then ./hello-world to execute that binary
